b6d0ca0ce439af4d1fef115444786956ca1f8168
Franco Viccia <francoviccia@gmail.com>
2020-08-05 12:51:36 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//Let the user go forward or backward with the arrow keys.
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "37") {
+    plusSlides(-1);
+  } else if (e.keyCode == "39") {
+    plusSlides(1);
+  }
+}
+
+//Keep the slides running.
+setInterval(plusSlides, 5000, 1);
